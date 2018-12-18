f2e871a24ee500b3fbd748059a52afc20f69f62b
Krishna Naidoo <krishna@krish-macbook.local>
2018-12-18 19:51:28 +0000

    update

--- test/test_splinter.py
+++ test/test_splinter.py
@@ -3,9 +3,9 @@ import wrapper as wrap
 
 path = '/share/data1/knaidoo/splinter_libraries/pylib/wrapper/test'
 root = 'splinter_test'
-redshift = [0., 0.5, 1., 2.]
-steps = [5, 5, 5, 5]
+redshift = [0.5]
+steps = [20]
 
-mgp = wrap.MGPicola(n_grid=64)
+mgp = wrap.MGPicola(n_grid=256, n_mesh=3*256)
 mgp.set_up(path, root, redshift, steps, get_pk=True, get_haloes=True)
 mgp.run()
